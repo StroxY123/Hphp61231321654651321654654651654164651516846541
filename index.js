443e764ba5c074905810bd0d47ea503ca3c7365c
StroxY123 <69604304+StroxY123@users.noreply.github.com>
2020-08-24 00:10:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -660,7 +660,30 @@ message.channel.send(embed).then(async msg => {
   }
 });
 
+client.on('message', message => {
+  if (message.content.startsWith(`${prefix}uptime`)) {
+  const upembed = new discord.RichEmbed()
+    let uptimesec = (bot.uptime / 3600);
+    let uptimehours = Math.floor(uptimesec / 3600);
+    let uptimeDays = Math.floor(uptimesec / 86400);
 
+    uptimesec %= 3600;
+
+    let uptimeMins = Math.floor(uptimesec / 60);
+    let uptimeSecTotal = Number.parseInt(uptimesec % 60);
+
+    const sec = `${uptimeSecTotal} másodperc`;
+    const min = `${uptimeMins} perc`;
+    const hour = `${uptimehours} óra`;
+    const days = `${uptimeDays} nap`;
+
+    let embed = new Discord.RichEmbed()
+    .setTitle('🕛Bot Uptime🕛')
+    .setDescription(`${uptimeDays?days:''} ${uptimehours?hour:''} ${uptimeMins?min:''} ${uptimeSecTotal?sec:''}`)
+    .setColor(szinek.aqua)
+    message.channel.send(upembed).catch(console.error)
+  }
+});
 client.on("message", message => {
   if (message.content.toLowerCase() === prefix + 'sad') {
     const embed = new discord.RichEmbed()
